e54352a4c7c0701c400e45013abdf83139c9a91d
travisbuel <travisbuel@gmail.com>
2020-12-30 16:57:02 -0600

    Initial Push

---
+++ alert.js
@@ -0,0 +1,6 @@
+'use strict';
+
+function check() {
+  document.getElementById('f1').innerHTML = document.calc.valor.value;
+  document.getElementByID('f2').innerHTML = document.calc.multiplier.value;
+}
