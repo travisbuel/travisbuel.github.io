ac7b3ee7b1326fc7070abbf7c21d45b9b85282ff
travisbuel <travisbuel@gmail.com>
2021-01-09 19:15:55 -0600

    Cleanup of Unused files, rework of index

---
+++ script.js
@@ -0,0 +1,58 @@
+function check() {
+	// Set variables
+	const valorMax = 2000;
+	const valorLegend = 1800;
+	var valorVal = parseInt(document.calc1.valor.value, 10);
+	var multiplier = parseInt(document.calc2.multiplier.value, 10);
+
+	// Check for errors
+
+	if (
+		valorVal >= 2000 ||
+		typeof valorVal !== "number" ||
+		Number.isNaN(valorVal)
+	) {
+		document.getElementById("error").style.display = "";
+		document.getElementById("results").style.display = "none";
+
+		console.log("oh fuqq something broke");
+	} else {
+		document.getElementById("difference").innerHTML =
+			Math.round(valorMax - valorVal) + " valor";
+		document.getElementById("percentage").innerHTML =
+			Math.round(valorVal / 20) + "%";
+		document.getElementById("progress-bar-valor-percent").style.width =
+			Math.round(valorVal / 20) + "%";
+		document.getElementById("result-high").innerHTML = Math.round(
+			(valorMax - valorVal) / multiplier
+		);
+		document.getElementById("result-low").innerHTML = Math.round(
+			(valorMax - valorVal) / multiplier / 4
+		);
+		document.getElementById("result-average").innerHTML = Math.round(
+			Math.round(
+				Math.round((valorMax - valorVal) / multiplier) +
+					Math.round((valorMax - valorVal) / multiplier / 4)
+			) / 2
+		);
+
+		document.getElementById("legend-low").innerHTML = Math.round(
+			(valorLegend - valorVal) / multiplier / 4
+		);
+		document.getElementById("legend-high").innerHTML = Math.round(
+			(valorLegend - valorVal) / multiplier
+		);
+
+		document.getElementById("legend-average").innerHTML = Math.round(
+			Math.round(
+				Math.round((valorLegend - valorVal) / multiplier) +
+					Math.round((valorLegend - valorVal) / multiplier / 4)
+			) / 2
+		);
+
+		document.getElementById("results").style.display = "";
+		document.getElementById("error").style.display = "none";
+
+		console.log("all good");
+	}
+}
