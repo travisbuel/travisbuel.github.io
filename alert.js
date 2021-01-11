1509dce74a1842724e92b9efd66de6c10206caab
travisbuel <travisbuel@gmail.com>
2021-01-11 16:52:05 -0600

    WebApp Integration

--- alert.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-function check() {
-  document.getElementById('f1').innerHTML = document.calc.valor.value;
-  document.getElementByID('f2').innerHTML = document.calc.multiplier.value;
-}
