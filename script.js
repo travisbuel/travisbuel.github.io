9fb567d9ca8edffff59299890e12c23bce8d0cca
travisbuel <travisbuel@gmail.com>
2021-01-10 21:45:13 -0600

    Add Footer / Dark Mode

--- script.js
+++ script.js
@@ -56,3 +56,12 @@ function check() {
 		console.log("all good");
 	}
 }
+
+// Select the button
+const btn = document.querySelector(".btn-toggle");
+
+// Listen for a click on the button
+btn.addEventListener("click", function () {
+	// Then toggle (add/remove) the .dark-theme class to the body
+	document.getElementById("body-wrapper").classList.toggle("dark-theme");
+});
